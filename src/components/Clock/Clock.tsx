687039ce417da7d697371c666bfedc8ff722b442
Xir <prssergey@gmail.com>
2025-06-07 16:29:48 +0300

    feat: clock (local)

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,26 @@
+import { TZDate } from "@date-fns/tz";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import { timezone, clock } from "./Clock.module.scss";
+
+const Clock = () => {
+  const [time, setTime] = useState<TZDate>(new TZDate());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new TZDate());
+    }, 1000);
+    () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div>
+      <span className={timezone}>{format(time, "z")}</span>
+      <p className={clock}>{format(time, "HH:mm:ss")}</p>
+    </div>
+  );
+};
+
+export default Clock;
