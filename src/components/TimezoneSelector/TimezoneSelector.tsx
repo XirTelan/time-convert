90ad91c2a88e4291aac180ac2165abdd999b60aa
Xir <prssergey@gmail.com>
2025-06-08 15:02:31 +0300

    feat: switch betweed modes

--- src/components/TimezoneSelector/TimezoneSelector.tsx
+++ src/components/TimezoneSelector/TimezoneSelector.tsx
@@ -1,20 +1,32 @@
-import DropDown from "@/shared/ui/DropDown/DropDown";
+import { DropDown } from "@/shared/ui";
 import useTimeStore from "@/hooks/useTimeStore";
+import { useMemo } from "react";
+import { commonTimeZones } from "@/shared/contants/timezones";
 
-const TimezoneSelector = ({ to = false }: { to?: boolean }) => {
+export const TimezoneSelector = ({ to = false }: { to?: boolean }) => {
+  const mode = useTimeStore((state) => state.mode);
   const timeZone = useTimeStore((state) =>
     to ? state.toTimeZone : state.fromTimeZone
   );
   const updateTimeZone = useTimeStore((state) => state.updateTimeZone);
 
+  const locations = useMemo(() => {
+    return Intl.supportedValuesOf("timeZone").map((tz) => ({
+      label: tz.replaceAll("/", " | ").replaceAll("_", " "),
+      value: tz,
+    }));
+  }, []);
+
+  const handleSelect = (value: string) => {
+    updateTimeZone(value, to);
+  };
+
   return (
     <DropDown
       defaultValue={timeZone}
       label={to ? "To:" : "From:"}
-      options={Intl.supportedValuesOf("timeZone")}
-      onSelect={(value: string) => updateTimeZone(value, to)}
+      options={mode === "timezone" ? commonTimeZones : locations}
+      onSelect={handleSelect}
     />
   );
 };
-
-export default TimezoneSelector;
---
+++ src/shared/contants/timezones.ts
@@ -0,0 +1,29 @@
+export const commonTimeZones = [
+  { label: "UTC", value: "UTC" },
+
+  { label: "EST", value: "America/New_York" },
+  { label: "MST", value: "America/Denver" },
+  { label: "PST", value: "America/Los_Angeles" },
+
+  { label: "GMT", value: "Europe/London" },
+  { label: "CET", value: "Europe/Paris" },
+  { label: "EET", value: "Europe/Bucharest" },
+
+  { label: "IST", value: "Asia/Kolkata" },
+  { label: "JST", value: "Asia/Tokyo" },
+  { label: "KST", value: "Asia/Seoul" },
+
+  { label: "AEST", value: "Australia/Sydney" },
+  { label: "ACST", value: "Australia/Adelaide" },
+  { label: "AWST", value: "Australia/Perth" },
+
+  { label: "AST", value: "Asia/Riyadh" },
+  { label: "IRST", value: "Asia/Tehran" },
+  { label: "GST", value: "Asia/Dubai" },
+
+  { label: "NZST", value: "Pacific/Auckland" },
+  { label: "HST", value: "Pacific/Honolulu" },
+
+  { label: "CST (US)", value: "America/Chicago" },
+  { label: "CST (China)", value: "Asia/Shanghai" },
+];
