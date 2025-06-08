79096cacb6f663f46f35983d4edde8183675a76e
Xir <prssergey@gmail.com>
2025-06-08 15:02:04 +0300

    feat: footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { wrapper } from "./Footer.module.scss";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={wrapper}>
+      <FaGithub />
+      <a
+        href="https://github.com/XirTelan/time-convert"
+        rel="nofollow noreferrer noopener"
+      >
+        GitHub Page
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
