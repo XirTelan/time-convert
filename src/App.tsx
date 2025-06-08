23f705844963d1d46a21b9447e5e8f2d7a66a330
Xir <prssergey@gmail.com>
2025-06-08 15:04:15 +0300

    refactor: app.tsx import, visual, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import Footer from "./layout/Footer/Footer";
 import Header from "./layout/Header/Header";
 
-import { container } from "./App.module.scss";
-import Clock from "./components/Clock/Clock";
-import Convert from "./components/Convert/Convert";
-import ModeSwitch from "./components/ModeSwitch/ModeSwitch";
-import ResultBlock from "./components/ResultBlock/ResultBlock";
+import {
+  container,
+  convert,
+  clockRow,
+  blockA,
+  blockB,
+  blockC,
+} from "./App.module.scss";
+
+import {
+  Clock,
+  ModeSwitch,
+  ResultBlock,
+  TimeInput,
+  TimezoneSelector,
+} from "./components";
+
+import { Card } from "./shared/ui";
 
 const App = () => {
-  console.log("app rendered");
   return (
     <>
       <Header />
       <main className={container}>
-        <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+        <div className={clockRow}>
           <Clock />
-          <ResultBlock />
         </div>
         <ModeSwitch />
-        <Convert />
+        <div className={convert}>
+          <div className={blockA}>
+            <Card>
+              <div>
+                <TimezoneSelector />
+                <TimezoneSelector to />
+              </div>
+            </Card>
+          </div>
+          <div className={blockB}>
+            <Card>
+              <TimeInput />
+            </Card>
+          </div>
+          <div className={blockC}>
+            <ResultBlock />
+          </div>
+        </div>
       </main>
       <Footer />
     </>
